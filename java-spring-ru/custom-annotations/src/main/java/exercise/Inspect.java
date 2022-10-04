0957068b055dae88f1ac0e770d124ca6e683c43c
@hexlet/cli <support@hexlet.io>
2022-10-04 14:56:52 +0300

    submit java-spring-ru/custom-annotations

---
+++ java-spring-ru/custom-annotations/src/main/java/exercise/Inspect.java
@@ -0,0 +1,15 @@
+package exercise;
+
+import java.lang.annotation.Target;
+import java.lang.annotation.Retention;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.RetentionPolicy;
+
+// BEGIN
+@Target(value = ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Inspect {
+    String level() default "debug";
+}
+
+// END
